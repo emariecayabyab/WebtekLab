885e6e2d555d8325060ed89843911fc105a62120
2158615 <2158615@slu.edu.ph>
2017-07-10 15:17:47 +0800

    Add files via upload
    search servelet

---
+++ Search.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.underscore;
+
+import com.mysql.jdbc.Connection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author s326lab
+ */
+@WebServlet(name = "Search", urlPatterns = {"/Search"})
+public class Search extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        	String searchUser = request.getParameter("searchUser");
+                
+                 PrintWriter pw = response.getWriter();
+				
+	try{
+                            
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/underscore", "root", "");
+            PreparedStatement ps = conn.prepareStatement("select * from users where firstname=? or lastname=? or idno=?");
+            
+            ps.setString(1, searchUser);
+            ps.setString(2, searchUser);
+            ps.setString(3, searchUser);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.first()) {
+                pw.print("Test");
+                
+            } else {
+                pw.print("Didn't Work");
+            }
+           
+        } catch(Exception e){
+            		
+	}
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
